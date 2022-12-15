65395bbb4a4a2f6b3ba07ed3623e522d51259ef8
ps-iria <ps.iria@yandex.ru>
2022-12-15 16:39:43 +0700

    15.3

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,12 +17,39 @@ resource "yandex_vpc_network" "default" {
 
 #2
 
-# resource "yandex_vpc_subnet" "public" {
-#   v4_cidr_blocks = ["172.31.32.0/19"]
-#   zone           = var.zone
-#   network_id     = yandex_vpc_network.default.id
-#   route_table_id = yandex_vpc_route_table.rt.id
-# }
+resource "yandex_vpc_subnet" "public" {
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  zone           = var.zone
+  network_id     = yandex_vpc_network.default.id
+  # route_table_id = yandex_vpc_route_table.rt.id
+}
+
+resource "yandex_compute_instance" "vm-0" {
+  name = "nat-instance"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+    ip_address = "192.168.10.254"
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    serial-port-enable = 1
+  }
+
+}
 
 # resource "yandex_vpc_gateway" "nat_gateway" {
 #   name = "gateway"
@@ -35,8 +62,8 @@ resource "yandex_vpc_network" "default" {
 
 #   static_route {
 #     destination_prefix = "0.0.0.0/0"
-#     next_hop_address   = "172.31.32.1"
-#     # gateway_id         = yandex_vpc_gateway.nat_gateway.id
+#     next_hop_address   = "192.168.10.254"
+#     gateway_id         = yandex_vpc_gateway.nat_gateway.id
 #   }
 # }
 
@@ -73,16 +100,16 @@ resource "yandex_vpc_network" "default" {
 #3
 
 resource "yandex_vpc_subnet" "private" {
-  v4_cidr_blocks = ["172.31.96.0/19"]
-  zone           = var.zone
+  v4_cidr_blocks = ["192.168.20.0/24"]
+  zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.default.id
   route_table_id = yandex_vpc_route_table.rt-private.id
 }
 
-resource "yandex_vpc_gateway" "private" {
-  name = "gateway"
-  shared_egress_gateway {}
-}
+# resource "yandex_vpc_gateway" "private" {
+#   name = "gateway"
+#   shared_egress_gateway {}
+# }
 
 resource "yandex_vpc_route_table" "rt-private" {
   name       = "route-table"
@@ -90,7 +117,37 @@ resource "yandex_vpc_route_table" "rt-private" {
 
   static_route {
     destination_prefix = "0.0.0.0/0"
-    next_hop_address   = "172.31.32.1"
-    # gateway_id         = yandex_vpc_gateway.nat_gateway.id
+    next_hop_address   = "192.168.10.254"
+    # gateway_id         = yandex_vpc_gateway.private.id
   }
+}
+
+data "yandex_compute_image" "ubuntu-20-04" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-20-04.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    serial-port-enable = 1
+  }
+
 }
